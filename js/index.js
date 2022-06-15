71e180e84934a17d670f1894ec9ca1171661365f
Eva Reitzel <evareitzel@gmail.com>
2022-06-15 00:00:49 -0400

    Done.

--- js/index.js
+++ js/index.js
@@ -1,45 +1,154 @@
+const form = document.getElementById('github-form')
+form.addEventListener('submit', handleSubmit)
 
-// Elements
-const searchInput = document.getElementById('search');
-// console.log(searchInput);
-const submitBtn = document.getElementById('github-form');
-// getElementsByName('submit')[0];
-console.log(submitBtn);
-submitBtn.addEventListener('submit', userSearch) 
-
-const userList = document.querySelector('#user-list');
-// userListContainer.append(userList);
-  
-// header: Accept: application/vnd.github.v3+json
-
-
-function userSearch(event){
-  event.preventDefault();
-//grab text input
-  console.log(searchInput.value);
-  fetch(`https://api.github.com/search/users?q=${searchInput.value}`) 
-  // console.log(`https://api.github.com/search/users?q=${searchInput}`);// append it to fetch "http://"
-  // .then(resp => resp.json())
-  .then(resp => resp.json())
-  // .then(resp => resp.items[0])
-  .then(resp => {
-    for (let i = 0; i < 3; i++) {
-      const result = document.createElement('li');
-      userList.append(result); 
-  
-      const avatar   = resp.items[i].avatar_url; // avatar.value
-      const login    = resp.items[i].login;
-      const htmlText = resp.items[i].html_url;
-      const html = login.link(htmlText); 
-
-      const avatarImg = document.createElement('img');
-      avatarImg.innerHTML = avatar.value;
-      userList.append(avatarImg); 
-
-      result.innerText = `${avatarImg} | ${login} | ${html}\n\n`;
+const button = document.createElement('button')
+button.textContent = 'Search for Users'
+const breakinpage = document.createElement('br')
+document.querySelector('h2').append(breakinpage, button)
+button.addEventListener('click', handleToggleClick)
+function handleSubmit(e) {
+    e.preventDefault()
+    document.querySelector('#user-list').innerHTML = ''
+    document.querySelector('#repos-list').textContent = ''
+    if (button.innerText === 'Search for Users') {
+        fetchInfo(e.target.search.value)
     }
-  })
+    if (button.innerText === 'Search for Repos') {
+        fetchRepoSearch(e.target.search.value)
+    }
 }
+function fetchInfo(searchedName) {
+    fetch(`https://api.github.com/search/users?q=${searchedName}`, {
+        headers: {
+            Accept: 'application/vnd.github.v3+json'
+        }
+    })
+    .then (resp => resp.json())
+    .then (data => {
+        console.log(data)
+        document.querySelector('#user-list').innerHTML = ''
+        data.items.forEach(renderInfo)
+    })
+}
+
+function renderInfo(userInfo) {
+  document.querySelector('#repos-list').textContent = ''
+
+  console.log(userInfo)
+  //name
+  const name = document.createElement('h2')
+  name.textContent = userInfo.login
+  name.addEventListener('click', () => fetchRepos(userInfo.login))
+
+  //avatar
+  const avatar = document.createElement('img')
+  avatar.src = userInfo.avatar_url
+
+  //profile link
+  const profile = document.createElement('a')
+  profile.textContent = "Profile Link"
+  profile.href = userInfo.html_url
+
+  //break 
+  const breakx = document.createElement('br')
+
+
+  document.querySelector('#user-list').append(name, profile, breakx, breakx, avatar)
+
+  
+}
+
+
+
+// // Elements
+// const searchInput = document.getElementById('search');
+// // console.log(searchInput);
+// const submitBtn = document.getElementById('github-form');
+// // getElementsByName('submit')[0];
+// console.log(submitBtn);
+// submitBtn.addEventListener('submit', userSearch) 
+// const userList = document.querySelector('#user-list');
+  
+// // header: Accept: application/vnd.github.v3+json
+
+
+// function userSearch(event){
+//   event.preventDefault();
+// //grab text input
+//   console.log(searchInput.value);
+//   fetch(`https://api.github.com/search/users?q=${searchInput.value}`) 
+//   // console.log(`https://api.github.com/search/users?q=${searchInput}`);// append it to fetch "http://"
+//   // .then(resp => resp.json())
+
+//   // header {
+//   //   header: Accept: application/vnd.github.v3+json
+//   // }
+
+//   .then(resp => resp.json())
+//   // .then(resp => resp.items[0])
+//   .then(resp => {
+//     forEach(let i = 0; i < 100; i++) {
+//       const result = document.createElement('li');
+//       userList.append(result); 
+  
+//       const avatar   = resp.items[i].avatar_url; // avatar.value
+//       const login    = resp.items[i].login;
+//       const htmlText = resp.items[i].html_url;
+//       const html     = login.link(htmlText); 
+
+//       const avatarImg = document.createElement('img');
+//       avatarImg.innerHTML = avatar.value;
+
+//       result.innerText = `${avatarImg} | ${login} | ${html}\n\n`;
+//     }
+//   })
+// }
+
+
+
+      //////////////////////////////////////////////////////////
+      
+// // Elements
+// const searchInput = document.getElementById('search');
+// // console.log(searchInput);
+// const submitBtn = document.getElementById('github-form');
+// // getElementsByName('submit')[0];
+// console.log(submitBtn);
+// submitBtn.addEventListener('submit', userSearch) 
+// const userList = document.querySelector('#user-list');
+  
+// // header: Accept: application/vnd.github.v3+json
+
+
+// function userSearch(event){
+//   event.preventDefault();
+// //grab text input
+//   console.log(searchInput.value);
+//   fetch(`https://api.github.com/search/users?q=${searchInput.value}`) 
+//   // console.log(`https://api.github.com/search/users?q=${searchInput}`);// append it to fetch "http://"
+//   // .then(resp => resp.json())
+//   header {
+//     header: Accept: application/vnd.github.v3+json
+//   }
+//   .then(resp => resp.json())
+//   // .then(resp => resp.items[0])
+//   .then(resp => {
+//     forEach(let i = 0; i < 100; i++) {
+//       const result = document.createElement('li');
+//       userList.append(result); 
+  
+//       const avatar   = resp.items[i].avatar_url; // avatar.value
+//       const login    = resp.items[i].login;
+//       const htmlText = resp.items[i].html_url;
+//       const html = login.link(htmlText); 
+
+//       const avatarImg = document.createElement('img');
+//       avatarImg.innerHTML = avatar.value;
+
+//       result.innerText = `${avatarImg} | ${login} | ${html}\n\n`;
+//     }
+//   })
+// }
 
   // .then(resp=> console.log(resp.items[0])); // slice, map, forEach (appends to DOM)
       
